1da6701fbd03f395c332c221cce44396089a9cb6
Pat Riehecky <riehecky@fnal.gov>
2016-04-18 17:42:35 -0500

    More fixes for scoping

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -1,11 +1,10 @@
 # == Class secrets::repo
 # mostly it just runs create_resources
 class secrets::repo (
-  $secrets_repos  = $secrets::secrets_repos,
-  $repo_defaults = $secrets::repo_defaults,
+  $secrets_repos = $::secrets::secrets_repos,
+  $repo_defaults = $::secrets::repo_defaults,
 ) {
 
   create_resources('secrets::resources::repo', $secrets_repos, $repo_defaults)
 
 }
-
